025ce860725d213725bf881580eb5b51b4abd72f
gordeyst <gordeystahovets@gmail.com>
2021-09-21 15:18:19 +0300

    feat(components): configure app components for dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box } from "@material-ui/core";
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import { blue } from "@material-ui/core/colors";
+import { observer } from "mobx-react-lite";
+import { useStore } from "./common/stores/Store";
+import { StartPage } from "./pages/start-page/StartPage";
+import { useEffect } from "react";
+import { IUserThemeSettings } from "./common/stores/CommonStore";
 
-function App() {
+const App = observer(() => {
+  const {
+    commonStore: { userThemeSettings, setUserThemeSettings },
+  } = useStore();
+
+  const muiTheme = createTheme({
+    palette: {
+      type: userThemeSettings.isDarkMode ? "dark" : "light",
+      primary: {
+        main: userThemeSettings.isDarkMode ? blue["A100"] : blue["A400"],
+      },
+    },
+  });
+
+  useEffect(() => {
+    const userThemeSettings: IUserThemeSettings = JSON.parse(
+      localStorage.getItem("userThemeSettings")!
+    );
+
+    setUserThemeSettings(userThemeSettings);
+  }, [setUserThemeSettings]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={muiTheme}>
+      <Box
+        style={{
+          backgroundColor: userThemeSettings.isDarkMode ? "#0e0e0e" : "#f5f5f5",
+        }}
+      >
+        <StartPage />
+      </Box>
+    </ThemeProvider>
   );
-}
+});
 
 export default App;
