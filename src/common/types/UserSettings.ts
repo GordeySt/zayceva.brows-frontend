3d3e28992fe31866114da2e092d71cbb425d0805
gordeyst <gordeystahovets@gmail.com>
2021-09-29 12:39:18 +0300

    feat(types): add types for user settings and custom themes

---
+++ src/common/types/UserSettings.ts
@@ -0,0 +1,24 @@
+﻿export type CustomTheme = {
+  name: string;
+  type: string;
+  palette: {
+    type: string;
+    primary: {
+      main: string;
+      light: string;
+      dark: string;
+    };
+    background: {
+      paper: string;
+      default: string;
+    };
+    text: {
+      primary: string;
+      secondary: string;
+    };
+  };
+};
+
+export type UserThemeSettings = {
+  themeType: string;
+};
