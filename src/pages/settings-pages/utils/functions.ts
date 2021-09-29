00d5a708c9a4c7e0da36aee081dc9abf2f8ec441
gordeyst <gordeystahovets@gmail.com>
2021-09-29 12:40:19 +0300

    feat: add utils functions for settings

---
+++ src/pages/settings-pages/utils/functions.ts
@@ -0,0 +1,26 @@
+import {
+  BACKGROUND_COLORS_DEFAULT,
+  BACKGROUND_COLORS_PAPER,
+  PaletteType,
+  THEME_NAMES,
+  THEME_PRIMARY_COLORS,
+  THEME_TEXT_COLORS,
+  THEME_TYPES,
+} from "../../../common/constants/themeConstants";
+import { CustomTheme } from "../../../common/types/UserSettings";
+
+export const makeCustomThemeFromThemeType = (
+  type: PaletteType
+): CustomTheme => ({
+  name: THEME_NAMES[type],
+  type,
+  palette: {
+    type: THEME_TYPES[type],
+    primary: THEME_PRIMARY_COLORS[type],
+    background: {
+      paper: BACKGROUND_COLORS_PAPER[type],
+      default: BACKGROUND_COLORS_DEFAULT[type],
+    },
+    text: THEME_TEXT_COLORS[type],
+  },
+});
