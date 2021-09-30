71693d71479362ff78b491d48f7affb55e6c639b
gordeyst <gordeystahovets@gmail.com>
2021-09-30 21:38:07 +0300

    feat(routes): add default route and new properties to the routes

--- src/common/config/routes.ts
+++ src/common/config/routes.ts
@@ -1,6 +1,7 @@
 import AboutPage from "../../pages/about-page/AboutPage";
 import AppearancePage from "../../pages/settings-pages/AppearancePage";
 import SettingsMenuPage from "../../pages/settings-pages/SettingsMenuPage";
+import { TestPage } from "../../pages/TestPage";
 import {
   ABOUT_ROUTE,
   APPEARANCE_SETTINGS_ROUTE,
@@ -10,21 +11,37 @@ import {
 export type Route = {
   path: string | string[];
   component: () => JSX.Element;
+  showBottomBar?: boolean;
+  showGoBackButton?: boolean;
+  title: string;
   exact?: boolean;
 };
 
 export const routes: Route[] = [
   {
+    path: "/",
+    component: TestPage,
+    title: "zayceva.brows",
+    showBottomBar: true,
+    exact: true,
+  },
+  {
     path: SETTINGS_MENU_ROUTE,
     component: SettingsMenuPage,
+    showBottomBar: true,
     exact: true,
+    title: "Настройки",
   },
   {
     path: APPEARANCE_SETTINGS_ROUTE,
     component: AppearancePage,
+    showGoBackButton: true,
+    title: "Внешний вид",
   },
   {
     path: ABOUT_ROUTE,
     component: AboutPage,
+    showGoBackButton: true,
+    title: "Обо мне",
   },
 ];
