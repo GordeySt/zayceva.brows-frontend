d5d457e306633246ae883e640998cbcede00d05c
gordeyst <gordeystahovets@gmail.com>
2021-10-01 19:24:34 +0300

    feat(mobx-stores): register modal and blacklist stores

--- src/common/stores/Store.ts
+++ src/common/stores/Store.ts
@@ -1,12 +1,18 @@
 import { createContext, useContext } from "react";
+import BlacklistStore from "./BlacklistStore";
 import CommonStore from "./CommonStore";
+import ModalStore from "./ModalStore";
 
 interface Store {
   commonStore: CommonStore;
+  blacklistStore: BlacklistStore;
+  modalStore: ModalStore;
 }
 
 export const store: Store = {
   commonStore: new CommonStore(),
+  blacklistStore: new BlacklistStore(),
+  modalStore: new ModalStore(),
 };
 
 export const StoreContext = createContext(store);
