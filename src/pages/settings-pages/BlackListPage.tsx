ffc7f6980fc07c5cc0389b99128e08df89df5950
gordeyst <gordeystahovets@gmail.com>
2021-10-04 20:10:37 +0300

    feat: add initial black list users load on component mount

--- src/pages/settings-pages/BlackListPage.tsx
+++ src/pages/settings-pages/BlackListPage.tsx
@@ -1,13 +1,24 @@
+import { observer } from "mobx-react-lite";
+import { useEffect } from "react";
+import { useStore } from "../../common/stores/Store";
 import BlackListBox from "../../components/settings-pages/blacklist-page/BlackListBox";
 import { InfoBox } from "../../components/settings-pages/blacklist-page/InfoBox";
 
-const BlackListPage = () => {
+const BlackListPage = observer(() => {
+  const {
+    blacklistStore: { loadUsers, blockedUsers },
+  } = useStore();
+
+  useEffect(() => {
+    if (blockedUsers.length === 0) loadUsers();
+  }, []);
+
   return (
     <div style={{ display: "flex", justifyContent: "space-around" }}>
       <BlackListBox />
       <InfoBox />
     </div>
   );
-};
+});
 
 export default BlackListPage;
