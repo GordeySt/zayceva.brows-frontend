27f2e7f811e06c2c894e2d89794ba1bf6435d0b4
gordeyst <gordeystahovets@gmail.com>
2021-10-04 18:45:14 +0300

    fix!: fix bug with displaying default mobx store value after refreshing the page (not good final solution)

--- src/components/common/App.tsx
+++ src/components/common/App.tsx
@@ -1,7 +1,7 @@
 import { createTheme, ThemeProvider } from "@mui/material";
 import { observer } from "mobx-react-lite";
 import { useStore } from "../../common/stores/Store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
 import AppLayout from "./AppLayout";
 import { UserSettings } from "../../common/types/UserSettings";
@@ -10,8 +10,9 @@ import { getUserSettingsFromLocalStorage } from "../../common/utils/localStorage
 
 const App = observer(() => {
   const {
-    commonStore: { setUserSettings, storeTheme },
+    commonStore: { setUserSettings, storeTheme, userSettings },
   } = useStore();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const userSettings: UserSettings = getUserSettingsFromLocalStorage();
@@ -19,9 +20,28 @@ const App = observer(() => {
     if (userSettings) {
       setUserSettings(userSettings);
     }
+
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 300);
   }, [setUserSettings]);
 
   const muiTheme = createTheme(storeTheme);
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          backgroundColor:
+            userSettings.themeType === "dark" ? "#0e0e0e" : "#f5f5f5",
+        }}
+      ></div>
+    );
+  }
 
   return (
     <ThemeProvider theme={muiTheme}>
