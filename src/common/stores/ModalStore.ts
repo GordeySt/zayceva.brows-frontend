416114824a33c5df8fd3fee71511df5c1d33e23b
Gordey Stakhovets <gordeystahovets@gmail.com>
2021-10-12 16:31:55 +0300

    refactor(mobx-stores): add additional properties to the modal object

--- src/common/stores/ModalStore.ts
+++ src/common/stores/ModalStore.ts
@@ -3,22 +3,25 @@ import { makeAutoObservable } from "mobx";
 interface Modal {
   open: boolean;
   body: JSX.Element | null;
+  fullWidth: boolean;
+  maxWidth?: string;
 }
 
 export default class ModalStore {
   modal: Modal = {
     open: false,
     body: null,
+    fullWidth: false,
   };
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  openModal = (content: JSX.Element) => {
+  openModal = (content: JSX.Element, fullWidth: boolean, maxWidth?: string) => {
     this.modal.open = true;
     this.modal.body = content;
-    console.log(content);
+    this.modal.fullWidth = fullWidth;
   };
 
   closeModal = () => {
