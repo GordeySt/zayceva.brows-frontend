b6bd2071c6f973266d33759a4c8f45d27b34c8a4
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-06-16 14:56:59 +0300

    feat: add react-final-form package to login form component

--- src/components/login-page/LoginForm.tsx
+++ src/components/login-page/LoginForm.tsx
@@ -5,7 +5,6 @@ import {
   Grid,
   IconButton,
   InputAdornment,
-  OutlinedInput,
   TextField,
   Typography,
 } from "@mui/material";
@@ -13,8 +12,8 @@ import { alpha, Theme } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import { observer } from "mobx-react-lite";
 import { useTranslation } from "react-i18next";
-import VisibilityIcon from "@material-ui/icons/Visibility";
-import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 import { useState } from "react";
 import {
   MAX_TABLET_WIDTH,
@@ -22,6 +21,8 @@ import {
 } from "../../common/constants/AdaptiveConstants";
 import { useHistory } from "react-router-dom";
 import { SIGN_UP_ROUTE } from "../../common/constants/RoutesConstants";
+import { Form, Field } from "react-final-form";
+import { LoadingButton } from "@mui/lab";
 
 const useStyles = makeStyles((theme: Theme) => ({
   rootCard: {
@@ -58,92 +59,122 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
+interface ILoginFormValues {
+  mail: string;
+  password: string;
+}
+
 const LoginForm = observer(() => {
   const classes = useStyles();
   const history = useHistory();
   const [showPassword, setShowPassword] = useState(false);
   const { t } = useTranslation();
 
-  const handleLoginSubmit = (e: any) => {
-    e.preventDefault();
+  const onSubmit = async (values: ILoginFormValues) => {
+    const sleep = (ms: number) =>
+      new Promise((resolve) => setTimeout(resolve, ms));
+    await sleep(3000);
 
-    console.log(e.target.email.value);
+    console.log(values);
   };
 
   return (
     <Card className={classes.rootCard}>
-      <form onSubmit={handleLoginSubmit}>
-        <Grid container direction="column">
-          <Grid item>
-            <Typography
-              className={classes.cardTitle}
-            >{t`pages.signInPage.sectionTitle`}</Typography>
-          </Grid>
-          <Grid item className={classes.gridItem}>
-            <Typography
-              className={classes.inputLabel}
-            >{t`pages.signInPage.mailLabel`}</Typography>
-            <TextField
-              autoFocus
-              autoComplete="email"
-              name="email"
-              size="small"
-              fullWidth
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item style={{ paddingTop: "13px" }}>
-            <Typography
-              className={classes.inputLabel}
-            >{t`pages.signInPage.passwordLabel`}</Typography>
-            <OutlinedInput
-              autoComplete="current-password"
-              name="password"
-              type={showPassword ? "text" : "password"}
-              fullWidth
-              size="small"
-              margin="dense"
-              endAdornment={
-                <InputAdornment position="end" style={{ marginRight: -8 }}>
-                  <IconButton
-                    className={classes.iconButton}
-                    onClick={() => setShowPassword((prev) => !prev)}
-                  >
-                    {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
-          </Grid>
-          <Grid item className={classes.gridItem}>
-            <Button
-              className={classes.button}
-              disableElevation
-              fullWidth
-              variant="contained"
-              color="primary"
-              type="submit"
-            >
-              {t`pages.signInPage.sectionTitle`}
-            </Button>
-          </Grid>
-          <Grid item className={classes.gridItem}>
-            <Divider>{t`pages.signInPage.dividerTitle`}</Divider>
-          </Grid>
-          <Grid item className={classes.gridItem}>
-            <Button
-              onClick={() => history.push(SIGN_UP_ROUTE)}
-              className={classes.button}
-              disableElevation
-              fullWidth
-              variant="outlined"
-              color="primary"
-            >
-              {t`pages.signInPage.signUpButtonText`}
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit, submitting }) => (
+          <form onSubmit={handleSubmit} noValidate>
+            <Grid container direction="column">
+              <Grid item>
+                <Typography
+                  className={classes.cardTitle}
+                >{t`pages.signInPage.sectionTitle`}</Typography>
+              </Grid>
+              <Grid item className={classes.gridItem}>
+                <Typography
+                  className={classes.inputLabel}
+                >{t`pages.signInPage.mailLabel`}</Typography>
+                <Field name="email">
+                  {({ input }) => (
+                    <TextField
+                      {...input}
+                      autoFocus
+                      autoComplete="email"
+                      size="small"
+                      fullWidth
+                      variant="outlined"
+                    />
+                  )}
+                </Field>
+              </Grid>
+              <Grid item style={{ paddingTop: "13px" }}>
+                <Typography
+                  className={classes.inputLabel}
+                >{t`pages.signInPage.passwordLabel`}</Typography>
+                <Field name="password">
+                  {({ input }) => (
+                    <TextField
+                      {...input}
+                      autoComplete="current-password"
+                      type={showPassword ? "text" : "password"}
+                      fullWidth
+                      size="small"
+                      margin="dense"
+                      InputProps={{
+                        endAdornment: (
+                          <InputAdornment
+                            position="end"
+                            style={{ marginRight: -8 }}
+                          >
+                            <IconButton
+                              className={classes.iconButton}
+                              onClick={() => setShowPassword((prev) => !prev)}
+                            >
+                              {showPassword ? (
+                                <VisibilityIcon />
+                              ) : (
+                                <VisibilityOffIcon />
+                              )}
+                            </IconButton>
+                          </InputAdornment>
+                        ),
+                      }}
+                    />
+                  )}
+                </Field>
+              </Grid>
+              <Grid item className={classes.gridItem}>
+                <LoadingButton
+                  className={classes.button}
+                  disableElevation
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  loading={submitting}
+                >
+                  {t`pages.signInPage.sectionTitle`}
+                </LoadingButton>
+              </Grid>
+              <Grid item className={classes.gridItem}>
+                <Divider>{t`pages.signInPage.dividerTitle`}</Divider>
+              </Grid>
+              <Grid item className={classes.gridItem}>
+                <Button
+                  onClick={() => history.push(SIGN_UP_ROUTE)}
+                  className={classes.button}
+                  disableElevation
+                  fullWidth
+                  variant="outlined"
+                  color="primary"
+                >
+                  {t`pages.signInPage.signUpButtonText`}
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        )}
+      />
     </Card>
   );
 });
