d31dfa0c1d170be0a1e9af32787faa0050448e45
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-08-04 17:53:27 +0300

    feat: add tips page and components

---
+++ src/components/tips-page/TipsCard.tsx
@@ -0,0 +1,62 @@
+import { Card, Grid } from "@mui/material";
+import { Theme } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import {
+  MAX_TABLET_WIDTH,
+  MIN_WIDTH,
+} from "../../common/constants/AdaptiveConstants";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  rootCard: {
+    padding: "20px",
+    borderRadius: "10px",
+    minHeight: "200px",
+    overflow: "hidden",
+
+    [theme.breakpoints.down(MAX_TABLET_WIDTH)]: {
+      width: "100%",
+    },
+    [theme.breakpoints.down(MIN_WIDTH)]: {
+      marginTop: "10px",
+    },
+  },
+  circle: {
+    display: "inline-block",
+    marginRight: "10px",
+    height: "10px",
+    width: "10px",
+    borderRadius: "30px",
+    backgroundColor: (props: IMUIStyleProps) => props.circleColor,
+  },
+  text: {
+    marginTop: "10px",
+  },
+}));
+
+interface IProps {
+  circleColor: string;
+  hashtag: string;
+  text: string;
+}
+
+interface IMUIStyleProps {
+  circleColor: string;
+}
+
+const TipsCard = ({ circleColor, hashtag, text }: IProps) => {
+  const classes = useStyles({ circleColor });
+
+  return (
+    <Grid item xs={12} md={6}>
+      <Card className={classes.rootCard}>
+        <div>
+          <i className={classes.circle}></i>
+          {hashtag}
+        </div>
+        <div className={classes.text}>{text}</div>
+      </Card>
+    </Grid>
+  );
+};
+
+export default TipsCard;
---
+++ src/pages/tips-page/TipsPage.tsx
@@ -0,0 +1,59 @@
+import { Container, Grid } from "@mui/material";
+import { Theme } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import {
+  MAX_TABLET_WIDTH,
+  MIN_WIDTH,
+} from "../../common/constants/AdaptiveConstants";
+import TipsCard from "../../components/tips-page/TipsCard";
+import { tips } from "./tips";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    marginBottom: theme.spacing(10),
+  },
+  rootCard: {
+    padding: "20px",
+    borderRadius: "10px",
+    minHeight: "200px",
+    overflow: "hidden",
+
+    [theme.breakpoints.down(MAX_TABLET_WIDTH)]: {
+      width: "100%",
+    },
+    [theme.breakpoints.down(MIN_WIDTH)]: {
+      marginTop: "10px",
+    },
+  },
+  circle: {
+    display: "inline-block",
+    marginRight: "10px",
+    height: "10px",
+    width: "10px",
+    borderRadius: "30px",
+  },
+  text: {
+    marginTop: "10px",
+  },
+}));
+
+const TipsPage = () => {
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.container}>
+      <Grid container spacing={2}>
+        {tips.map((tip, idx) => (
+          <TipsCard
+            key={idx}
+            circleColor={tip.circleColor}
+            hashtag={tip.hashtag}
+            text={tip.text}
+          />
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default TipsPage;
