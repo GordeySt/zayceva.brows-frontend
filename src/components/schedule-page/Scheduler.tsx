c263b55b2c7fd4c0cbbf51649f78cae143d97613
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-09-28 21:20:13 +0300

    feat(components): add functionality to set data in the scheduler

--- src/components/schedule-page/Scheduler.tsx
+++ src/components/schedule-page/Scheduler.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
     Scheduler as ReactScheduler,
     WeekView,
     DayView,
-    Appointments
+    Appointments,
+    Toolbar,
+    DateNavigator,
+    TodayButton
 } from '@devexpress/dx-react-scheduler-material-ui';
 import { Paper, useMediaQuery, useTheme } from "@mui/material";
 import { MAX_TABLET_WIDTH } from "../../common/constants/adaptiveConstants";
@@ -17,29 +20,42 @@ import {
 } from "./utils/schedulerTimeUtils";
 import { IAppointments } from "../../common/models/schedule";
 import { getUserSettingsFromLocalStorage } from "../../common/utils/localStorageUtils";
+import {  currentMockDate } from "../../pages/schedule-page/utils/schedulerMockData"
 
 interface IProps {
-    currentDate: Date;
     appointments: IAppointments[];
 }
 
-const Scheduler = ({ currentDate, appointments }: IProps) => {
+const Scheduler = ({ appointments }: IProps) => {
     const theme = useTheme();
     const matches = useMediaQuery(theme.breakpoints.down(MAX_TABLET_WIDTH));
+
+    const [schedulerData, setSchedulerData] = useState({
+        appointments,
+        currentDate: currentMockDate
+    });
+
+    const onCurrentDateChange = (currentDate: Date) => {
+        setSchedulerData(prevState => ({
+            appointments: [...prevState.appointments],
+            currentDate: currentDate
+        }))
+    }
 
     return (
         <Paper>
             <ReactScheduler
-                data={appointments}
+                data={schedulerData.appointments}
                 locale={getUserSettingsFromLocalStorage().language}
             >
                 <ViewState
-                    defaultCurrentDate={currentDate}
+                    defaultCurrentDate={schedulerData.currentDate}
+                    onCurrentDateChange={onCurrentDateChange}
                 />
                 {matches ?
                     <DayView
-                        startDayHour={getStartDayHourDay(appointments, currentDate)}
-                        endDayHour={getEndDayHourDay(appointments, currentDate)}
+                        startDayHour={getStartDayHourDay(appointments, schedulerData.currentDate)}
+                        endDayHour={getEndDayHourDay(appointments, schedulerData.currentDate)}
                     />
                     :
                     <WeekView
@@ -48,6 +64,9 @@ const Scheduler = ({ currentDate, appointments }: IProps) => {
                     />
                 }
                 <Appointments appointmentComponent={CustomAppointment}/>
+                <Toolbar />
+                <DateNavigator />
+                <TodayButton />
             </ReactScheduler>
         </Paper>
     );
