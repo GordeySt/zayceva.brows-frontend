06157e542aa717abac424b7b0a110479cf513b9c
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-09-28 17:47:51 +0300

    refactor(utils): refactor useRoute method

--- src/common/utils/routeUtils.ts
+++ src/common/utils/routeUtils.ts
@@ -1,12 +1,11 @@
-import { useLocation, matchPath } from "react-router-dom";
+import { matchPath, useLocation } from "react-router-dom";
 import { routes } from "../config/routes";
 
 export const useRoute = () => {
   const location = useLocation();
   const path = location.pathname;
-  const route = routes.find((e) =>
-    matchPath(path, { path: e.path, exact: true })
-  );
 
-  return route;
+  return routes.find((e) =>
+      matchPath(path, { path: e.path, exact: true })
+  );
 };
