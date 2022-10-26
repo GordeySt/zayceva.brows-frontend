eb023a499bf7f76c4734a03b8a0fc842b958c60c
Gordey Stakhovets <gordeystahovets@gmail.com>
2022-10-26 20:13:07 +0300

    fix: fix bug witl null object from local storage

--- src/components/schedule-page/Scheduler.tsx
+++ src/components/schedule-page/Scheduler.tsx
@@ -1,34 +1,38 @@
-import React, { useCallback, useState } from 'react';
-import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
+import React, { useCallback, useState } from "react";
 import {
-    Scheduler as ReactScheduler,
-    WeekView,
-    DayView,
-    Appointments,
-    Toolbar,
-    DateNavigator,
-    TodayButton,
-    AppointmentTooltip,
-    AppointmentForm,
-    ConfirmationDialog,
-    DragDropProvider
-} from '@devexpress/dx-react-scheduler-material-ui';
+  ViewState,
+  EditingState,
+  IntegratedEditing,
+} from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler as ReactScheduler,
+  WeekView,
+  DayView,
+  Appointments,
+  Toolbar,
+  DateNavigator,
+  TodayButton,
+  AppointmentTooltip,
+  AppointmentForm,
+  ConfirmationDialog,
+  DragDropProvider,
+} from "@devexpress/dx-react-scheduler-material-ui";
 import { Paper, useMediaQuery, useTheme } from "@mui/material";
 import {
-    APP_TOP_BAR_DEFAULT_HEIGHT,
-    APP_TOP_BAR_MOBILE_HEIGHT,
-    MAX_TABLET_WIDTH,
-    MIN_WIDTH
+  APP_TOP_BAR_DEFAULT_HEIGHT,
+  APP_TOP_BAR_MOBILE_HEIGHT,
+  MAX_TABLET_WIDTH,
+  MIN_WIDTH,
 } from "../../common/constants/adaptiveConstants";
 import {
-    getEndDayHourDay,
-    getEndDayHourWeek,
-    getStartDayHourDay,
-    getStartDayHourWeek
+  getEndDayHourDay,
+  getEndDayHourWeek,
+  getStartDayHourDay,
+  getStartDayHourWeek,
 } from "./utils/schedulerTimeUtils";
 import { IAppointments } from "../../common/models/schedule";
 import { getUserSettingsFromLocalStorage } from "../../common/utils/localStorageUtils";
-import { currentMockDate } from "../../pages/schedule-page/utils/schedulerMockData"
+import { currentMockDate } from "../../pages/schedule-page/utils/schedulerMockData";
 import { useStore } from "../../common/stores/Store";
 import AppointmentFormTextEditor from "./AppointmentFormTextEditor";
 import AppointmentFormLayout from "./AppointmentFormLayout";
@@ -39,100 +43,130 @@ import SchedulerDateTimePicker from "./SchedulerDateTimePicker";
 import CustomAppointment from "./CustomAppointment";
 
 interface IProps {
-    appointments: IAppointments[];
+  appointments: IAppointments[];
 }
 
 const Scheduler = ({ appointments }: IProps) => {
-    const {
-        userSettingsStore: { userSettings },
-    } = useStore();
-    const theme = useTheme();
-    const matchesTablets = useMediaQuery(theme.breakpoints.down(MAX_TABLET_WIDTH));
-    const matchesPhones = useMediaQuery(theme.breakpoints.down(MIN_WIDTH));
+  const {
+    userSettingsStore: { userSettings },
+  } = useStore();
+  const theme = useTheme();
+  const matchesTablets = useMediaQuery(
+    theme.breakpoints.down(MAX_TABLET_WIDTH)
+  );
+  const matchesPhones = useMediaQuery(theme.breakpoints.down(MIN_WIDTH));
 
-    const topBarHeight = matchesPhones ? APP_TOP_BAR_MOBILE_HEIGHT : APP_TOP_BAR_DEFAULT_HEIGHT;
+  const topBarHeight = matchesPhones
+    ? APP_TOP_BAR_MOBILE_HEIGHT
+    : APP_TOP_BAR_DEFAULT_HEIGHT;
 
-    const [schedulerAppointments, setSchedulerAppointments] = useState(appointments);
-    const [schedulerCurrentDate, setSchedulerCurrentDate] = useState(currentMockDate);
+  const [schedulerAppointments, setSchedulerAppointments] =
+    useState(appointments);
+  const [schedulerCurrentDate, setSchedulerCurrentDate] =
+    useState(currentMockDate);
 
-    const onCurrentDateChange = (currentDate: Date) => {
-        setSchedulerCurrentDate(currentDate);
-    }
+  const onCurrentDateChange = (currentDate: Date) => {
+    setSchedulerCurrentDate(currentDate);
+  };
 
-    const commitChanges = useCallback(({ added, changed, deleted }: any) => {
-        if (added) {
-            const startingAddedId = schedulerAppointments.length > 0 ?
-                schedulerAppointments[schedulerAppointments.length - 1].id + 1 : 0;
+  const commitChanges = useCallback(
+    ({ added, changed, deleted }: any) => {
+      if (added) {
+        const startingAddedId =
+          schedulerAppointments.length > 0
+            ? schedulerAppointments[schedulerAppointments.length - 1].id + 1
+            : 0;
 
-            setSchedulerAppointments([...schedulerAppointments, { id: startingAddedId, ...added }]);
+        setSchedulerAppointments([
+          ...schedulerAppointments,
+          { id: startingAddedId, ...added },
+        ]);
+      }
+
+      if (changed) {
+        setSchedulerAppointments(
+          schedulerAppointments.map((appointment) =>
+            changed[appointment.id]
+              ? { ...appointment, ...changed[appointment.id] }
+              : appointment
+          )
+        );
+      }
+
+      if (deleted !== undefined) {
+        setSchedulerAppointments(
+          schedulerAppointments.filter(
+            (appointment) => appointment.id !== deleted
+          )
+        );
+      }
+    },
+    [setSchedulerAppointments, schedulerAppointments]
+  );
+
+  return (
+    <Paper>
+      <ReactScheduler
+        height={document.documentElement.clientHeight - topBarHeight}
+        data={schedulerAppointments}
+        locale={
+          getUserSettingsFromLocalStorage()
+            ? getUserSettingsFromLocalStorage().language
+            : "ru"
         }
-
-        if (changed) {
-            setSchedulerAppointments(schedulerAppointments.map(appointment => (
-                changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment)));
-        }
-
-        if (deleted !== undefined) {
-            setSchedulerAppointments(schedulerAppointments.filter(appointment => appointment.id !== deleted));
-        }
-    }, [setSchedulerAppointments, schedulerAppointments]);
-
-    return (
-        <Paper>
-            <ReactScheduler
-                height={document.documentElement.clientHeight - topBarHeight}
-                data={schedulerAppointments}
-                locale={getUserSettingsFromLocalStorage().language}
-            >
-                <ViewState
-                    defaultCurrentDate={schedulerCurrentDate}
-                    onCurrentDateChange={onCurrentDateChange}
-                />
-                <EditingState
-                    onCommitChanges={commitChanges}
-                />
-                <IntegratedEditing />
-                {matchesTablets ?
-                    <DayView
-                        startDayHour={getStartDayHourDay(appointments, schedulerCurrentDate)}
-                        endDayHour={getEndDayHourDay(appointments, schedulerCurrentDate)}
-                    />
-                    :
-                    <WeekView
-                        startDayHour={getStartDayHourWeek(appointments)}
-                        endDayHour={getEndDayHourWeek(appointments)}
-                    />
-                }
-                <Toolbar />
-                <DateNavigator />
-                <TodayButton />
-                <ConfirmationDialog messages={getConfirmationDialogMessages(userSettings.language)} />
-                <Appointments appointmentComponent={CustomAppointment} />
-                <AppointmentTooltip
-                    showCloseButton
-                    showOpenButton
-                    showDeleteButton
-                    contentComponent={AppointmentTooltipContent}
-                />
-                <AppointmentForm
-                    layoutComponent={AppointmentFormLayout}
-                    booleanEditorComponent={AppointmentFormRadio}
-                    messages={getLabels(userSettings.language)}
-                    textEditorComponent={(props) =>
-                        <AppointmentFormTextEditor {...props} />
-                    }
-                    dateEditorComponent={(props) =>
-                        <SchedulerDateTimePicker
-                            {...props}
-                            locale={userSettings.language}
-                            matchesMinScreenWidth={matchesPhones}
-                        />
-                    }
-                />
-                {!matchesTablets && <DragDropProvider />}
-            </ReactScheduler>
-        </Paper>
-    );
+      >
+        <ViewState
+          defaultCurrentDate={schedulerCurrentDate}
+          onCurrentDateChange={onCurrentDateChange}
+        />
+        <EditingState onCommitChanges={commitChanges} />
+        <IntegratedEditing />
+        {matchesTablets ? (
+          <DayView
+            startDayHour={getStartDayHourDay(
+              appointments,
+              schedulerCurrentDate
+            )}
+            endDayHour={getEndDayHourDay(appointments, schedulerCurrentDate)}
+          />
+        ) : (
+          <WeekView
+            startDayHour={getStartDayHourWeek(appointments)}
+            endDayHour={getEndDayHourWeek(appointments)}
+          />
+        )}
+        <Toolbar />
+        <DateNavigator />
+        <TodayButton />
+        <ConfirmationDialog
+          messages={getConfirmationDialogMessages(userSettings.language)}
+        />
+        <Appointments appointmentComponent={CustomAppointment} />
+        <AppointmentTooltip
+          showCloseButton
+          showOpenButton
+          showDeleteButton
+          contentComponent={AppointmentTooltipContent}
+        />
+        <AppointmentForm
+          layoutComponent={AppointmentFormLayout}
+          booleanEditorComponent={AppointmentFormRadio}
+          messages={getLabels(userSettings.language)}
+          textEditorComponent={(props) => (
+            <AppointmentFormTextEditor {...props} />
+          )}
+          dateEditorComponent={(props) => (
+            <SchedulerDateTimePicker
+              {...props}
+              locale={userSettings.language}
+              matchesMinScreenWidth={matchesPhones}
+            />
+          )}
+        />
+        {!matchesTablets && <DragDropProvider />}
+      </ReactScheduler>
+    </Paper>
+  );
 };
 
 export default Scheduler;
