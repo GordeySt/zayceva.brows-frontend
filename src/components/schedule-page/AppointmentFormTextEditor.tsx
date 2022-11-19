8b7af5d2aaf23320432c864763fcfaa53fc9001e
Gordey <gordeystahovets@gmail.com>
2022-11-19 23:30:13 +0300

    feat: add uuid to services

--- src/components/schedule-page/AppointmentFormTextEditor.tsx
+++ src/components/schedule-page/AppointmentFormTextEditor.tsx
@@ -1,45 +1,58 @@
-import React from 'react';
+import React from "react";
 import { useTranslation } from "react-i18next";
-import { ListSubheader, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import {
+  ListSubheader,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 import { createService } from "../../pages/services-page/utils/mockServices";
 
 interface IProps {
-    readOnly?: boolean;
-    placeholder?: string;
-    value?: string | number;
-    onValueChange: (value: string) => void;
-    type?: "titleTextEditor" | "multilineTextEditor" | "ordinaryTextEditor" | "numberEditor";
+  readOnly?: boolean;
+  placeholder?: string;
+  value?: string | number;
+  onValueChange: (value: string) => void;
+  type?:
+    | "titleTextEditor"
+    | "multilineTextEditor"
+    | "ordinaryTextEditor"
+    | "numberEditor";
 }
 
-const AppointmentFormTextEditor = ({ onValueChange, value, type}: IProps) => {
-    const { t } = useTranslation();
+const AppointmentFormTextEditor = ({ onValueChange, value, type }: IProps) => {
+  const { t } = useTranslation();
 
-    const onSelectChange = (event: SelectChangeEvent) => {
-        onValueChange(event.target.value);
-    }
+  const onSelectChange = (event: SelectChangeEvent) => {
+    onValueChange(event.target.value);
+  };
 
-    if (type === 'multilineTextEditor') {
-        return null;
-    }
+  if (type === "multilineTextEditor") {
+    return null;
+  }
 
-    return (
-        <Select
-            style={{ width: '100%' }}
-            value={value as string}
-            onChange={onSelectChange}
-            id="grouped-select"
-            inputProps={{ 'aria-label': 'Without label' }}
-        >
-            <ListSubheader>{t`pages.servicesPage.categories.brows`}</ListSubheader>
-            {createService(t).brows.map(t => t.title).map((t, idx) => (
-                <MenuItem key={idx} value={t}>{t}</MenuItem>
-            ))}
-            <ListSubheader>{t`pages.servicesPage.categories.lashes`}</ListSubheader>
-            {createService(t).eyelashes.map(t => t.title).map((t, idx) => (
-                <MenuItem key={idx} value={t}>{t}</MenuItem>
-            ))}
-        </Select>
-    )
+  return (
+    <Select
+      style={{ width: "100%" }}
+      value={(value as string) ?? ""}
+      onChange={onSelectChange}
+      id="grouped-select"
+      inputProps={{ "aria-label": "Without label" }}
+    >
+      <ListSubheader>{t`pages.servicesPage.categories.brows`}</ListSubheader>
+      {createService(t).brows.map((t) => (
+        <MenuItem key={t.id} value={t.title}>
+          {t.title}
+        </MenuItem>
+      ))}
+      <ListSubheader>{t`pages.servicesPage.categories.lashes`}</ListSubheader>
+      {createService(t).eyelashes.map((t) => (
+        <MenuItem key={t.id} value={t.title}>
+          {t.title}
+        </MenuItem>
+      ))}
+    </Select>
+  );
 };
 
 export default AppointmentFormTextEditor;
--- src/pages/services-page/utils/mockServices.ts
+++ src/pages/services-page/utils/mockServices.ts
@@ -1,93 +1,70 @@
+import { v4 as uuid } from "uuid";
+
 export const createService = (t: any) => ({
-    brows: [
-        {
-            title: t('pages.servicesPage.servicesCard.browCorrection'),
-            price: 17,
-            description: t('pages.servicesPage.servicesCard.browCorrectionDesc'),
-            availableTime: [
-                new Date(2022, 9, 15, 12, 0),
-                new Date(2022, 9, 15, 14, 0),
-                new Date(2022, 9, 15, 16, 0),
-                new Date(2022, 9, 15, 18, 0)
-            ]
-        },
-        {
-            title: t('pages.servicesPage.servicesCard.browTinting'),
-            price: 25,
-            description: t('pages.servicesPage.servicesCard.browTintingDesc'),
-            availableTime: [
-                new Date(2022, 9, 15, 12, 0),
-                new Date(2022, 9, 15, 14, 0),
-                new Date(2022, 9, 15, 16, 0),
-                new Date(2022, 9, 15, 12, 0),
-            ]
-        },
-        {
-            title: t('pages.servicesPage.servicesCard.longTermStylingAndCorrection'),
-            price: 27,
-            description: t('pages.servicesPage.servicesCard.longTermStylingAndCorrectionDesc'),
-            availableTime: [
-                new Date(2022, 9, 15, 12, 0),
-                new Date(2022, 9, 15, 14, 0),
-                new Date(2022, 9, 15, 16, 0),
-            ]
-        },
-        {
-            title: t('pages.servicesPage.servicesCard.longTermStylingAndTinting'),
-            price: 30,
-            description: t('pages.servicesPage.servicesCard.longTermStylingAndTintingDesc'),
-            availableTime: [
-                new Date(2022, 9, 15, 12, 0),
-                new Date(2022, 9, 15, 14, 0),
-                new Date(2022, 9, 15, 16, 0),
-            ]
-        }
-    ],
-    eyelashes: [
-        {
-            title: 'Коррекция ресниц',
-            price: 17,
-            description: 'Коррекция проводится с ипользованием пинцета и воска.',
-            availableTime: [
-                new Date(2022, 9, 15, 12, 0),
-                new Date(2022, 9, 15, 14, 0),
-                new Date(2022, 9, 15, 16, 0),
-                new Date(2022, 9, 15, 18, 0)
-            ]
-        },
-        {
-            title: 'Окрашивание бровей c коррекцией',
-            price: 25,
-            description: 'Окрашивание проводится с помощью специальной краски, после чего делается коррекция. ' +
-                'Стоимость коррекции входит в стоимость окрашивания.',
-            availableTime: [
-                new Date(2022, 9, 15, 12, 0),
-                new Date(2022, 9, 15, 14, 0),
-                new Date(2022, 9, 15, 16, 0),
-                new Date(2022, 9, 15, 12, 0),
-            ]
-        },
-        {
-            title: 'Долговременная укладка с коррекцией',
-            price: 27,
-            description: 'Окрашивание проводится с помощью специальной краски, после чего делается коррекция. ' +
-                'Стоимость коррекции входит в стоимость окрашивания.',
-            availableTime: [
-                new Date(2022, 9, 15, 12, 0),
-                new Date(2022, 9, 15, 14, 0),
-                new Date(2022, 9, 15, 16, 0),
-            ]
-        },
-        {
-            title: 'Долговременная укладка с окрашиванием и коррекцией',
-            price: 30,
-            description: 'Окрашивание проводится с помощью специальной краски, после чего делается коррекция. ' +
-                'Стоимость коррекции входит в стоимость окрашивания.',
-            availableTime: [
-                new Date(2022, 9, 15, 12, 0),
-                new Date(2022, 9, 15, 14, 0),
-                new Date(2022, 9, 15, 16, 0),
-            ]
-        }
-    ]
+  brows: [
+    {
+      id: uuid(),
+      title: t("pages.servicesPage.servicesCard.browCorrection"),
+      price: 17,
+      description: t("pages.servicesPage.servicesCard.browCorrectionDesc"),
+      availableTime: [
+        new Date(2022, 9, 15, 12, 0),
+        new Date(2022, 9, 15, 14, 0),
+        new Date(2022, 9, 15, 16, 0),
+        new Date(2022, 9, 15, 18, 0),
+      ],
+    },
+    {
+      id: uuid(),
+      title: t("pages.servicesPage.servicesCard.browTinting"),
+      price: 25,
+      description: t("pages.servicesPage.servicesCard.browTintingDesc"),
+      availableTime: [
+        new Date(2022, 9, 15, 12, 0),
+        new Date(2022, 9, 15, 14, 0),
+        new Date(2022, 9, 15, 16, 0),
+        new Date(2022, 9, 15, 12, 0),
+      ],
+    },
+    {
+      id: uuid(),
+      title: t("pages.servicesPage.servicesCard.longTermStylingAndCorrection"),
+      price: 27,
+      description: t(
+        "pages.servicesPage.servicesCard.longTermStylingAndCorrectionDesc"
+      ),
+      availableTime: [
+        new Date(2022, 9, 15, 12, 0),
+        new Date(2022, 9, 15, 14, 0),
+        new Date(2022, 9, 15, 16, 0),
+      ],
+    },
+    {
+      id: uuid(),
+      title: t("pages.servicesPage.servicesCard.longTermStylingAndTinting"),
+      price: 30,
+      description: t(
+        "pages.servicesPage.servicesCard.longTermStylingAndTintingDesc"
+      ),
+      availableTime: [
+        new Date(2022, 9, 15, 12, 0),
+        new Date(2022, 9, 15, 14, 0),
+        new Date(2022, 9, 15, 16, 0),
+      ],
+    },
+  ],
+  eyelashes: [
+    {
+      id: uuid(),
+      title: t("pages.servicesPage.servicesCard.lashesLamination"),
+      price: 40,
+      description: "Коррекция проводится с ипользованием пинцета и воска.",
+      availableTime: [
+        new Date(2022, 9, 15, 12, 0),
+        new Date(2022, 9, 15, 14, 0),
+        new Date(2022, 9, 15, 16, 0),
+        new Date(2022, 9, 15, 18, 0),
+      ],
+    },
+  ],
 });
